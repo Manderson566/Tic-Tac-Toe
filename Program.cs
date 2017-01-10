7793102333beba4f218b2fd0f8a09eb9393f8c59
Michael Anderson <Micmail566@gmail.com>
2017-01-10 16:23:39 -0500

    Sort of working.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,42 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
+            {
+                string Instructions = "Test";
+               
+
+                Console.WriteLine(Instructions);
+            }
+            string[] board = new[] {
+                "______0", "______1", "______2", "______3", "______4", "______5", "______6", "______7", "______8"
+        };
+            {
+                int Player = int.Parse (Console.ReadLine());
+                board[Player] = "___X___";
+            }
+                int counter = 0;
+                foreach (string place in board)
+                    
+            {
+                Console.Write(" " + place);
+                if (counter == 2)
+                {
+                    Console.WriteLine("");
+                    counter = 0;
+
+                }
+                else
+                {
+                    counter++;
+                }
+
+
+                
+            }
         }
     }
 }
+
+        
+    
+
